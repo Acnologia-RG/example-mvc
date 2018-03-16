c3f4656546e2cb230d4109c39338faf5e0364edc
Maarten Nouwen <mnouwen@davinci.nl>
2018-03-16 11:35:21 +0100

    song/index nieuwe features

--- view/song/index.php
+++ view/song/index.php
@@ -5,6 +5,7 @@
 			<th>Artiest</th>
 			<th>Titel</th>
 			<th>Url</th>
+			<th colspan="2">Actie</th>
 		</tr>
 		
 		<?php foreach ($songs as $song) {
@@ -12,7 +13,9 @@
 			echo "<td>" . $song['song_id'] . "</td>";
 			echo "<td>" . $song['song_artist'] . "</td>";
 			echo "<td>" . $song['song_name'] . "</td>";
-			echo "<td>" . $song['song_url'] . "</td>";
+			echo "<td><a href=\"" . $song['song_url'] .  "\">" . $song['song_url'] . "</a></td>";
+			echo "<td><a href=\"" . URL . "song/edit/" . $song['song_id'] . "\">Wijzigen</a></td>";
+			echo "<td><a href=\"" . URL . "song/delete/" . $song['song_id'] . "\">Verwijderen</a></td>";
 			echo "</tr>";
 		}
 		?>
